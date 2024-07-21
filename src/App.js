126d1e4647315f073e3bccb2af33802ad3514ddb
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-07-21 13:58:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {
 	Group,
 	Card,
 	ActionIcon,
-	Code,
 } from '@mantine/core';
 import { useState, useRef, useEffect } from 'react';
 import { MoonStars, Sun, Trash } from 'tabler-icons-react';
@@ -16,7 +15,6 @@ import { MoonStars, Sun, Trash } from 'tabler-icons-react';
 import {
 	MantineProvider,
 	ColorSchemeProvider,
-	ColorScheme,
 } from '@mantine/core';
 import { useColorScheme } from '@mantine/hooks';
 import { useHotkeys, useLocalStorage } from '@mantine/hooks';
